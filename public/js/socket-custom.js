1e599cf22fae8ad4880d7267c39e36c05fcb2e01
Alexander <easuazab@co.ibm.com>
2019-04-21 21:51:27 -0500

    Primer commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,28 @@
+var socket = io();
+
+socket.on('connect', function() {
+
+    console.log('Conectado al servidor');
+
+});
+
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión al servidor');
+
+});
+
+//Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server', resp);
+});
+
+//Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
